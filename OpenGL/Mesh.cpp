f66386e3d73a03489b1419783d0a75fb21fc29cd
LanceAve <lanceave@protonmail.com>
2025-09-23 21:47:38 -0400

    modified mesh to have color information

--- OpenGL/Mesh.cpp
+++ OpenGL/Mesh.cpp
@@ -17,9 +17,16 @@ void Mesh::Create(Shader* _shader)
 {
 	m_shader = _shader;
 
-	m_vertexData = { -1.0f, -1.0f, 0.0f,
-					  1.0f, -1.0f, 0.0f,
-					  0.0f,  1.0f, 0.0f };
+	m_vertexData = { 
+		/*  Position  */  /*    RGBA Color    */
+		0.2f, 0.2f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+		0.3f, 0.9f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+		0.4f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 
+		0.7f, 0.8f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 
+		0.8f, 0.4f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 
+		1.0f, 0.6f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 
+		1.0f, 0.2f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 
+		1.5f, 0.6f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, };
 	glGenBuffers(1, &m_vertexBuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
 	glBufferData(GL_ARRAY_BUFFER, m_vertexData.size() * sizeof(float), m_vertexData.data(), GL_STATIC_DRAW);
