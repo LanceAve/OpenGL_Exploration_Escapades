fdc1e07d330a072eaaaec90231c2ea29135558bd
LanceAve <lanceave@protonmail.com>
2025-09-24 11:59:18 -0400

    modified mesh header file to include index buffer information

--- OpenGL/Mesh.h
+++ OpenGL/Mesh.h
@@ -2,6 +2,7 @@
 #define MESH_H
 
 #include "StandardIncludes.h"
+
 class Shader;
 
 class Mesh
@@ -18,10 +19,15 @@ public:
 									// so that they don't overlap unto each other
 
 private:
-	Shader* m_shader;	// then define the variable
-	GLuint m_vertexBuffer;
-	std::vector<GLfloat> m_vertexData;
-	glm::mat4 m_world;
+	Shader* m_shader;						// then define the variable
+	GLuint m_vertexBuffer;					// stored in GPU buffer
+	vector<GLubyte> m_indexBuffer;			// notice that it uses the GLubyte type (also index buffer gets stored in RAM as well)
+	vector<GLfloat> m_vertexData;			// store the vertex data in RAM
+	mat4 m_world;
 };
+
+// avoids redundant std:: after every method call
+using namespace std;
+using namespace glm;
 
 #endif // MESH_H
