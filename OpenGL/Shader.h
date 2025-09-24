dfdba1e6a10c7126f64dd1b8a8cb0ef83f27e2d1
LanceAve <lanceave@protonmail.com>
2025-09-23 21:36:44 -0400

    added color attributes definitions

--- OpenGL/Shader.h
+++ OpenGL/Shader.h
@@ -13,22 +13,25 @@ public:
 	// Accessors
 	GLuint GetProgramID() { return m_programID; }
 	GLuint GetAttrVertices() { return m_attrVertices; }
+	GLuint GetAttrColors() { return m_attrColors; }		// adding color accessor now
+	GLuint GetAttrWVP() { return m_attrWVP; }			// allow access to WVP
 
 	// Methods
 	void LoadShaders(const char* _vertexFilePath, const char* _fragementFilePath);
 	void Cleanup();
-	GLuint GetAttrWVP() { return m_attrWVP; }	// allow access to WVP
+	
 
 private:
 	// Methods
-	void CreateShaderProgram(const char* _vertexFilePath, const char* _fragmentFilePath);	// ID of out shader program
-	GLuint LoadShaderFile(const char* _filePath, GLenum _type);	// Handle for the attribute vertex buffer
+	void CreateShaderProgram(const char* _vertexFilePath, const char* _fragmentFilePath);	// ID of our shader program
+	GLuint LoadShaderFile(const char* _filePath, GLenum _type);								// Handle for the attribute vertex buffer
 	void LoadAttributes();
 	void EvaluateShader(int _infoLength, GLuint _id);
 
 	// Members
 	GLuint m_programID;				// ID of our shader program
 	GLuint m_attrVertices;			// Handle for the attribute vertex buffer
+	GLuint m_attrColors;			// Color attribute now sick 
 	GLuint m_attrWVP;
 	GLint m_result = GL_FALSE;
 	int	m_infoLogLength;
