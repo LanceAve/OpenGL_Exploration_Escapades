911b1049aeb3d121955810d012fef0f3626130b2
LanceAve <lanceave@protonmail.com>
2025-09-26 22:12:07 -0400

    fixed missing semicolon

--- OpenGL/GameController.cpp
+++ OpenGL/GameController.cpp
@@ -12,7 +12,7 @@ GameController::GameController()
 void GameController::Initialize()
 {
 	GLFWwindow* window = WindowController::GetInstance().GetWindow();	// Must call this first, since it creates the window
-	M_ASSERT(glewInit() == GLEW_OK, "Failed to initialize GLEW.")		// Initialize GLEW
+	M_ASSERT(glewInit() == GLEW_OK, "Failed to initialize GLEW.");		// Initialize GLEW
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);				// Make sure that we can capture the escape key if it's pressed
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);								// Changed to Black backgroun now (can be deduced using art program)
 	glEnable(GL_CULL_FACE);												// prevents face of mesh from being displayed on both sides
