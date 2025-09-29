49d5e49a036f262846305cf20b7ccb40ee15514b
LanceAve <lanceave@protonmail.com>
2025-09-29 17:40:37 -0400

    modfiied shader.h to have texture coordinate definitions

--- OpenGL/Shader.h
+++ OpenGL/Shader.h
@@ -13,8 +13,9 @@ public:
 	// Accessors
 	GLuint GetProgramID() { return m_programID; }
 	GLuint GetAttrVertices() { return m_attrVertices; }
-	GLuint GetAttrColors() { return m_attrColors; }		// adding color accessor now
-	GLuint GetAttrWVP() { return m_attrWVP; }			// allow access to WVP
+	GLuint GetAttrColors() { return m_attrColors; }			// adding color accessor now
+	GLuint GetAttrTexCoords() { return m_attrTexCoords; }	// accessor for texture coordinates
+	GLuint GetAttrWVP() { return m_attrWVP; }				// allow access to WVP
 
 	// Methods
 	void LoadShaders(const char* _vertexFilePath, const char* _fragementFilePath);
@@ -31,8 +32,10 @@ private:
 	// Members
 	GLuint m_programID;				// ID of our shader program
 	GLuint m_attrVertices;			// Handle for the attribute vertex buffer
-	GLuint m_attrColors;			// Color attribute now sick 
+	GLuint m_attrColors;			// Color attribute now sick
+	GLuint m_attrTexCoords;			// adding in texture coordinates member now
 	GLuint m_attrWVP;
+	GLuint m_sampler1;				// sampler for first example (more to come)
 	GLint m_result = GL_FALSE;
 	int	m_infoLogLength;
 };
