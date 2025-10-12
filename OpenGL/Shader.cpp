14f771b01e53d4d5ef7df984ed3e4b97597dffa0
LanceAve <lanceave@protonmail.com>
2025-10-11 22:49:48 -0400

    added handlers and initiailizer for YUV and invert

--- OpenGL/Shader.cpp
+++ OpenGL/Shader.cpp
@@ -11,6 +11,10 @@ Shader::Shader()
 	m_sampler1 = 0;					// *NEW* sampler 1
 	m_sampler2 = 0;					// *NEW* sampler 2 (yessssss)
 	m_attrWVP = 0;
+	m_uniformY = 0;
+	m_uniformU = 0;
+	m_uniformV = 0;
+	m_invert = 0;
 	m_result = GL_FALSE;
 	m_infoLogLength = 0;
 }
@@ -22,7 +26,11 @@ void Shader::LoadAttributes()
 	m_attrTexCoords = glGetAttribLocation(m_programID, "texCoords");	// *NEW* Get a handle for the texCoords buffer
 	m_attrWVP = glGetUniformLocation(m_programID, "WVP");				// Get a handle for the WVP matrix
 	m_sampler1 = glGetUniformLocation(m_programID, "sampler1");			// *NEW* Get a handle for texture sampler 1
-	m_sampler2 = glGetUniformLocation(m_programID, "sampler2");			// *NEW* Get a handle for texture sampler 2 
+	m_sampler2 = glGetUniformLocation(m_programID, "sampler2");			// *NEW* Get a handle for texture sampler 2
+	m_uniformY = glGetUniformLocation(m_programID, "uY");
+	m_uniformU = glGetUniformLocation(m_programID, "uU");
+	m_uniformV = glGetUniformLocation(m_programID, "uV");
+	m_invert = glGetUniformLocation(m_programID, "uInvert");
 }
 
 void Shader::EvaluateShader(int _infoLength, GLuint _id)
