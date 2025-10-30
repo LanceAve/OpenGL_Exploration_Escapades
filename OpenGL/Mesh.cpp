2d02b43a4eefabb86c58387ca2bb30d94b87c4b1
LanceAve <lanceave@protonmail.com>
2025-10-30 11:31:17 -0400

    decloupled Render method into smaller methods and removed index buffer use

--- OpenGL/Mesh.cpp
+++ OpenGL/Mesh.cpp
@@ -4,19 +4,30 @@
 
 Mesh::Mesh()
 {
+	// shader related
 	m_shader = nullptr;
+
+	// texture related
 	m_texture = { };
 	m_texture2 = { };
+
+	// buffer related
 	m_vertexBuffer = 0;
 	m_indexBuffer = 0;
-	m_position = { 0, 0, 0 };
+
+	// transformation and scale
+	m_position = { 0, 0, 0, };
 	m_rotation = { 0, 0, 0 };
-	m_world = mat4(1.0f);
+	m_scale = { 1, 1, 1 };
+	m_world = mat4();
+
+	// light related
+	m_lightPosition = { 0, 0, 0 };
+	m_lightColor = { 1, 1, 1 };		// default to white for now
+
 }
 
-Mesh::~Mesh()
-{
-}
+Mesh::~Mesh() { }
 
 // define what the create function does
 void Mesh::Create(Shader* _shader) 
@@ -35,6 +46,7 @@ void Mesh::Create(Shader* _shader)
 	// https://prideout.net/blog/old/archive/colors.php.html#Floats
 
 	// changing shape to a cube now! (hidden so it doesn't take too much space.)
+	#pragma region VertexData
 	m_vertexData = 
 	{
 		/*    Position    */  /*    Normals    */  /* Texture Coords */
@@ -80,37 +92,24 @@ void Mesh::Create(Shader* _shader)
 		-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  0.0f,
 		-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
 	};
-
+	#pragma endregion
 	glGenBuffers(1, &m_vertexBuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
 	glBufferData(GL_ARRAY_BUFFER, m_vertexData.size() * sizeof(float), m_vertexData.data(), GL_STATIC_DRAW);
-
-	// adding the index data as well
-	m_indexData = {
-		2, 0, 3, 2, 1, 0
-	};
-
-	glGenBuffers(1, &m_indexBuffer);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_indexBuffer);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, m_indexData.size() * sizeof(GLubyte), m_indexData.data(), GL_STATIC_DRAW);
 }
 
 // define what the cleanup function does (used for garbage collection)
 void Mesh::Cleanup()
 {
 	glDeleteBuffers(1, &m_vertexBuffer);
-	glDeleteBuffers(1, &m_indexBuffer);
 	m_texture.Cleanup();
 	m_texture2.Cleanup();					// gotta clean up that text2 babe
 }
 
 // this time define what the Render function does
 // and define the world view stuff
-void Mesh::Render(mat4 _wvp)
-{
-	glUseProgram(m_shader->GetProgramID());						// use our shader instead of the built-in one
-	m_shader->SetVec3("DiffuseColor", { 1.0, 0.0f, 0.0f });		// render both texture and diffuse color (set to red)
-	
+void Mesh::BindAttributes()
+{	
 	// 1st attribute buffer : vertices
 	glEnableVertexAttribArray(m_shader->GetAttrVertices());
 	glVertexAttribPointer(
@@ -141,14 +140,8 @@ void Mesh::Render(mat4 _wvp)
 		8 * sizeof(float),				// stride (now modified to 8 floats per vertex def)
 		(void*)(6 * sizeof(float)));	// array buffer offset
 	
-	// 4th attribute : WVP (World-View-Projection)
-	m_rotation.y += 0.0008f;	// rotate the object by 5 units on the y axis
-	mat4 transform = rotate(_wvp, m_rotation.y, vec3(0, 1, 0));
-	glUniformMatrix4fv(m_shader->GetAttrWVP(), 1, GL_FALSE, &transform[0][0]);
-	
-	// Bind both the vertex and index to reduce memory use
-	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);			// vertex
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_indexBuffer);	// index
+	// Bind vertex buffer
+	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
 
 	// binding texture one to texture unit 0 (up to 16 in OpenGL)
 	glActiveTexture(GL_TEXTURE0);
@@ -159,10 +152,41 @@ void Mesh::Render(mat4 _wvp)
 	glActiveTexture(GL_TEXTURE1);
 	glBindTexture(GL_TEXTURE_2D, m_texture2.GetTexture());	// Bind the texture now too
 	glUniform1i(m_shader->GetSampler2(), 1);				// notice how it's 1 now.
+}
+
+void Mesh::CalculateTransform()
+{
+	m_world = translate(mat4(1.0f), m_position);
+	m_world = rotate(m_world, m_rotation.y, vec3(0, 1, 0));
+	m_world = scale(m_world, m_scale);
+}
+
+void Mesh::SetShaderVariables(mat4 _pv)
+{
+	m_shader->SetMat4("WVP", _pv * m_world);
+	m_shader->SetMat4("World", m_world);
+
+	m_shader->SetVec3("DiffuseColor", { 1.0f, 1.0f, 1.0f });
+	
+	m_shader->SetVec3("AmbientLight", { 0.1f, 0.1f, 0.1f });
+	m_shader->SetVec3("LightPosition", m_lightPosition);
+	m_shader->SetVec3("LightColor", m_lightColor);
+}
+
+void Mesh::Render(mat4 _pv)
+{
+	glUseProgram(m_shader->GetProgramID());
+
+	m_rotation.y += 0.0008f;
+
+	CalculateTransform();
+	SetShaderVariables(_pv);
+	BindAttributes();
 
 	// draw a triangle element
-	glDrawArrays(GL_TRIANGLES, 0, m_vertexData.size());
-	
+	GLsizei vertexCount = m_vertexData.size() / 8;
+	glDrawArrays(GL_TRIANGLES, 0, vertexCount);
+
 	// turn off normal, vertex and texture coordinate data after draw call
 	glDisableVertexAttribArray(m_shader->GetAttrNormals());
 	glDisableVertexAttribArray(m_shader->GetAttrVertices());
