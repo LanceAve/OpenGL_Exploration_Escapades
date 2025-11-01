2bc1a3bae693fee124a068bb060d8df1c55e5706
LanceAve <lanceave@protonmail.com>
2025-10-31 20:21:31 -0400

    changed lighting cube position

--- OpenGL/GameController.cpp
+++ OpenGL/GameController.cpp
@@ -38,7 +38,7 @@ void GameController::RunGame()
 	// Mesh creation
 	m_meshLight = Mesh();
 	m_meshLight.Create(&m_shaderColor);
-	m_meshLight.SetPosition({ 1.0f, 0.5f, 0.5f });
+	m_meshLight.SetPosition({ 1.0f, -0.5f, 0.0f });
 	m_meshLight.SetScale({ 0.1f, 0.1f, 0.1f });
 
 	m_meshBox = Mesh();
