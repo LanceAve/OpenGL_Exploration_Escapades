0f44961669985d7f4cbaac4e2f44fa3f404f81d8
LanceAve <lanceave@protonmail.com>
2025-10-31 20:16:53 -0400

    replaced individual sampler accessors with SetTextureSampler method

--- OpenGL/Shader.h
+++ OpenGL/Shader.h
@@ -16,13 +16,12 @@ public:
 	GLuint GetAttrColors() { return m_attrColors; }			// adding color accessor now
 	GLuint GetAttrNormals() { return m_attrNormals; }		// accessor for the normals
 	GLuint GetAttrTexCoords() { return m_attrTexCoords; }	// accessor for texture coordinates
-	GLuint GetSampler1() { return m_sampler1; }				// accessor for the sampler 1
-	GLuint GetSampler2() { return m_sampler2; }				// accessor for sampler 2 (to layer textures on each other)
 	GLuint GetAttrWVP() { return m_attrWVP; }				// allow access to WVP
 
 	// Methods
 	void LoadShaders(const char* _vertexFilePath, const char* _fragementFilePath);
 	void Cleanup();
+	void SetTextureSampler(const char* _name, GLuint _texUnit, int _texUnitID, int _value);
 	void SetFloat(const char* _name, float _value);
 	void SetVec3(const char* _name, vec3 _value);
 	void SetMat4(const char* _name, mat4 _value);
@@ -41,8 +40,6 @@ private:
 	GLuint m_attrNormals;			// Normals attribute
 	GLuint m_attrTexCoords;			// adding in texture coordinates member now
 	GLuint m_attrWVP;
-	GLuint m_sampler1;				// sampler for first example (more to come)
-	GLuint m_sampler2;				// sampler for second example (foreshadowing paid off :v )
 	GLint m_result = GL_FALSE;
 	int	m_infoLogLength;
 };
