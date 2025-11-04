ec905e3f0cc174af1917325f0a345793678c6335
LanceAve <lanceave@protonmail.com>
2025-11-04 13:08:31 -0500

    changed for loop so it no longer spawns cubes in a random direction, and creates a wall.

--- OpenGL/GameController.cpp
+++ OpenGL/GameController.cpp
@@ -39,21 +39,23 @@ void GameController::RunGame()
 	// Mesh creation
 	m_meshLight = Mesh();
 	m_meshLight.Create(&m_shaderColor);
-	m_meshLight.SetPosition({ 1.0f, -0.5f, 0.0f });
+	m_meshLight.SetPosition({ 0.5f, 0.0f, -0.5f });
 	m_meshLight.SetScale({ 0.1f, 0.1f, 0.1f });
 
-	// randomly spawn 10 cubes with random scale and location
-	for (int count = 0; count < 10; count++)
+	// modifying the for loop such that it creates a wall of cubes rather than spawning them randomly
+	for (int col = 0; col < 10; col++)
 	{
-
-		Mesh box = Mesh();
-		box.Create(&m_shaderDiffuse);
-		box.SetLightColor({ 0.5f, 0.9f, 0.5f });
-		box.SetLightPosition(m_meshLight.GetPosition());
-		box.SetCameraPosition(m_camera.GetPosition());
-		box.SetScale({ 0.3f, 0.3f, 0.3f });
-		box.SetPosition({ linearRand(-1.0f, 1.0f), linearRand(-1.0f, 1.0f), linearRand(-1.0f, 1.0f) });
-		m_meshBoxes.push_back(box);
+		for (int count = 0; count < 10; count++)
+		{
+			Mesh box = Mesh();
+			box.Create(&m_shaderDiffuse);
+			box.SetLightColor({ 1.0f, 1.0f, 1.0f });
+			box.SetLightPosition(m_meshLight.GetPosition());
+			box.SetCameraPosition(m_camera.GetPosition());
+			box.SetScale({ 0.1f, 0.1f, 0.1f });
+			box.SetPosition({ 0.0f, -0.5f + (float)count / 10.0f, -0.2f + (float)col / 10.0f });
+			m_meshBoxes.push_back(box);
+		}
 	}
 
 	do
